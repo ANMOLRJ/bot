683f5322f8f5bd10139e2c26ca0125b5e3e21cf6
anmol r j <anmolrj89@gmail.com>
2022-08-13 10:45:19 +0530

    initial commit

---
+++ long_responses.py
@@ -0,0 +1,10 @@
+import random
+
+R_EATING = "I don't like eating anything because I'm a bot obviously!"
+R_ADVICE = "If I were you, I would go to the internet and type exactly what you wrote there!"
+
+
+def unknown():
+    response = ["  Please contact on 8069-505-505 for more assistance. Our support team will guide you further.@#%EndRes%#@$ thankyou #@$!  "][
+        random.randrange(1)]
+    return response
